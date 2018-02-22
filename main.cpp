8c1a4175a13904dcfad07367131c23db904b4ba7
sekcayala <aayala39@mail.csuchico.edu>
2018-02-21 19:37:17 -0800

    Trial 1

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//Author: Alejandro Ayala
 #include<iostream>
 #include<string>
 using namespace std;
@@ -7,13 +8,36 @@ int main()
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
-
+             
+     do 
+   {
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
+              
 
+  if ( input == 'A' || input == 'a' )
+  {
+  cout<<"What is the item? \n";
+  cin>>list[numItems+1];
+  }
+  
+ // list[0] = 
+ // list[1] = 
+ // list[2] =
+ // list[3] =
+ // list[4] =
+ // list[5] =
+
+  if ( numItems > 5)
+  {
+  cout<<"You'll need a bigger list!\n";
+  }
+  }
+
+  while (!( input == 'Q' || input == 'q' ));
   return 0;
 }
